b85b89a0236d2f8a85b57cb3d91415c7c93ab52f
Suarry <susan900000@gmail.com>
2021-07-15 03:09:02 +0900

    단계별 문제풀이 Step_9

--- Step_9/1929.py
+++ Step_9/1929.py
@@ -1,17 +1,13 @@
-##시간초과/업뎃 예정
-
 start, end = map(int, input().split())
-is_sosu = [True for i in range(start, end+1)]
 
 for i in range(start, end+1):
-    if is_sosu[i-start]:
-        print("now", i)
+    if i>2:
+        is_sosu = True
         for j in range(2, int(i**0.5)+2):
-            if j==int(i**0.5)+1:
-                print(i)
-                for k in range(i*2, end, i):
-                    is_sosu[k-start]=False
-                    print(is_sosu)
-            elif i%j==0:
-                is_sosu[i-start]=False
+            if i%j==0:
+                is_sosu = False
                 break
+        if is_sosu:
+            print(i)
+    elif i==2:
+        print(2)
