8fac58780a913c99b8428411ff55cb7fec1ef3be
Suarry <susan900000@gmail.com>
2021-08-24 01:03:33 +0900

    단계별 문제풀이 Step_22

---
+++ Step_22/11279.py
@@ -0,0 +1,12 @@
+import heapq
+import sys
+
+test = int(sys.stdin.readline())
+max_heap = []
+
+for _ in range(test):
+    num = int(sys.stdin.readline())
+    if num==0:
+        print(-heapq.heappop(max_heap) if len(max_heap)>0 else "0")
+    else:
+        heapq.heappush(max_heap, -num)
