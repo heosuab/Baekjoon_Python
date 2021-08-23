a359c35d39908fe36a1d6d3608296a7ecdb5b07b
Suarry <susan900000@gmail.com>
2021-08-24 01:32:13 +0900

    단계별 문제풀이 Step_22

--- Step_22/11286.py
+++ Step_22/11286.py
@@ -7,6 +7,6 @@ min_heap = []
 for _ in range(test):
     num = int(sys.stdin.readline())
     if num==0:
-        print(heapq.heappop(min_heap) if len(min_heap)>0 else "0")
+        print(heapq.heappop(min_heap)[1] if len(min_heap)>0 else "0")
     else:
-        heapq.heappush(min_heap, num)
+        heapq.heappush(min_heap, (abs(num), num))
