8da06bb9b33b8f59174fcf5a12f26442ce033427
Suarry <susan900000@gmail.com>
2021-09-14 11:19:49 +0900

    단계별 문제풀이 Step_23

--- Step_23/11066.py
+++ Step_23/11066.py
@@ -5,6 +5,10 @@ for _ in range(test):
     num = int(sys.stdin.readline())
     matrix = [[0]*(num+1) for _ in range(num+1)]
     file_size = list(map(int, sys.stdin.readline().split()))
+    sum_size = []
+
+    for i in range(num):
+        sum_size.append(sum(file_size[:i+1]))
 
     for i in range(1, num):
         for j in range(1, num-i+1):
@@ -13,7 +17,10 @@ for _ in range(test):
                 matrix[j][j+i] = file_size[j-1] + file_size[j]
                 continue
             for k in range(j, j+i):
-                case.append(matrix[j][k] + matrix[k+1][j+i] + sum(file_size[j-1:j+i]))
+                if j-2<0:
+                    case.append(matrix[j][k] + matrix[k+1][j+i] + sum_size[j+i-1])
+                else:
+                    case.append(matrix[j][k] + matrix[k+1][j+i] + sum_size[j+i-1] - sum_size[j-2])
             matrix[j][j+i] = min(case)
     
     print(matrix[1][num])
